84a560df2b31aeccbd1a9fc60b2389156f912dbd
speedAngel <yeal136@163.com>
2019-03-27 17:03:55 +0800

    Signed-off-by: speedAngel <yeal136@163.com>

--- Eureka-Client1/src/main/java/com/my/EurekaClientApplication.java
+++ Eureka-Client1/src/main/java/com/my/EurekaClientApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @EnableEurekaClient
 @SpringBootApplication
@@ -15,7 +14,7 @@ public class EurekaClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
-
+	
 	@Value("${server.port}")
 	private String port;
 	
@@ -23,4 +22,6 @@ public class EurekaClientApplication {
 	public String index() {
 		return "service for port:" + port;
 	}	
+	
+
 }
